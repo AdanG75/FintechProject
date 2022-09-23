b6a6ace3ec42088cbc165799268292e2823d490f
AdanG75 <ansg75@gmail.com>
2022-09-23 00:50:51 -0500

    Add terms of service and privacy policy

--- routers/home.py
+++ routers/home.py
@@ -24,3 +24,31 @@ async def home(
         {"request": request}
     )
 
+
+@router.get(
+    path='/privacy',
+    status_code=status.HTTP_200_OK,
+    response_class=HTMLResponse
+)
+async def privacy_policy(
+        request: Request
+):
+    return templates.TemplateResponse(
+        "privacy_policy.html",
+        {"request": request}
+    )
+
+
+@router.get(
+    path='/terms-of-service',
+    status_code=status.HTTP_200_OK,
+    response_class=HTMLResponse
+)
+async def terms_of_service(
+        request: Request
+):
+    return templates.TemplateResponse(
+        "terms_of_service.html",
+        {"request": request}
+    )
+
