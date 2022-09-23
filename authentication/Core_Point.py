aea7c9307ae517b5c702c2e5c657afa1f80bfadd
AdanG75 <ansg75@gmail.com>
2022-09-22 23:17:53 -0500

    Delete old authentication module

--- authentication/Core_Point.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-from Characteristic_Point import Characteristic_Point
-
-class Core_Point(Characteristic_Point):
-    def __init__(self, posy, posx, angle, point_type) -> None:
-        super().__init__(posy, posx, angle, point_type)
