1946512fdd8cc84f31b703c68a869aa2410b05a8
AdanG75 <ansg75@gmail.com>
2023-01-11 15:59:58 -0600

    Add id_type into TokenBase class

--- schemas/token_base.py
+++ schemas/token_base.py
@@ -6,6 +6,7 @@ class TokenBase(BaseModel):
     token_type: str = Field("bearer", min_length=3, max_length=15)
     user_id: int = Field(..., gt=0)
     type_user: str = Field(..., min_length=5, max_length=8)
+    id_type: str = Field(..., min_length=20, max_length=40)
 
     class Config:
         orm_mode = True
